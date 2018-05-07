d750b7421272c34de1b87c2efec97d27f2ab1045
liuqin <q.liu@protruly.com.cn>
2018-05-07 11:00:50 +0800

    modify for 360 wifi 2

--- mybin/load_wifi.sh
+++ mybin/load_wifi.sh
@@ -1,20 +1,28 @@
 #!/bin/sh
+
+ifname="enp4s0"
+
 #remove the driver before
 ifconfig ra0 down
-rmmod mt7601Usta
+#rmmod mt7601Usta
+rmmod mt7601u
+
 #add new ap driver
 modprobe rtutil7601Uap
 modprobe mt7601Uap
 modprobe rtnet7601Uap
+
 #set ip
 ifconfig ra0 up
 ifconfig ra0 192.168.199.1
+
 #dhcp the network
 service isc-dhcp-server restart
 dhcpd ra0
+
 #make if forward work from eth0
 echo 1 | sudo tee /proc/sys/net/ipv4/ip_forward
 iptables -t filter -F
 iptables -t nat -F
-iptables -t nat -A POSTROUTING -o eth0 -j MASQUERADE
+iptables -t nat -A POSTROUTING -o $ifname -j MASQUERADE
 
--- mybin/unload_wifi.sh
+++ mybin/unload_wifi.sh
@@ -1,5 +1,11 @@
 #!/bin/sh
+
+# unload wifi ap drive
 ifconfig ra0 down
 rmmod rtnet7601Uap 
 rmmod mt7601Uap 
 rmmod rtutil7601Uap
+
+# load wifi drive
+#modprobe mt7601Usta
+modprobe mt7601u
